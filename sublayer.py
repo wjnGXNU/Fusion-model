9fa897b14c6dd8ee0677159df4cdda3a0eebd276
wjnxbb <48880752+wjnxbb@users.noreply.github.com>
2024-04-10 22:07:21 +0800

    Update sublayer.py

--- sublayer.py
+++ sublayer.py
@@ -6,7 +6,6 @@ from config import SIG_LEN
 
 
 class MultiHeadAttention(nn.Module):
-    ''' Multi-Head Attention module '''
 
     def __init__(self, n_head, d_model, d_k, d_v, dropout=0.1):
         super().__init__()
@@ -22,7 +21,6 @@ class MultiHeadAttention(nn.Module):
         nn.init.normal_(self.w_ks.weight, mean=0, std=np.sqrt(2.0 / (d_model + d_k)))
         nn.init.normal_(self.w_vs.weight, mean=0, std=np.sqrt(2.0 / (d_model + d_v)))
 
-        #加入了SDPAttention
         self.attention = SDPAttention(temperature=np.power(d_k, 0.5))
         self.layer_norm = nn.LayerNorm(d_model)
 
@@ -44,15 +42,15 @@ class MultiHeadAttention(nn.Module):
         k = self.w_ks(k).view(sz_b, len_k, n_head, d_k)
         v = self.w_vs(v).view(sz_b, len_v, n_head, d_v)
 
-        q = q.permute(2, 0, 1, 3).contiguous().view(-1, len_q, d_k)  # (n*b) x lq x dk
-        k = k.permute(2, 0, 1, 3).contiguous().view(-1, len_k, d_k)  # (n*b) x lk x dk
-        v = v.permute(2, 0, 1, 3).contiguous().view(-1, len_v, d_v)  # (n*b) x lv x dv
+        q = q.permute(2, 0, 1, 3).contiguous().view(-1, len_q, d_k)
+        k = k.permute(2, 0, 1, 3).contiguous().view(-1, len_k, d_k)
+        v = v.permute(2, 0, 1, 3).contiguous().view(-1, len_v, d_v)
 
-        mask = mask.repeat(n_head, 1, 1)  # (n*b) x .. x ..
+        mask = mask.repeat(n_head, 1, 1)  
         output, attn = self.attention(q, k, v, mask=mask)
 
         output = output.view(n_head, sz_b, len_q, d_v)
-        output = output.permute(1, 2, 0, 3).contiguous().view(sz_b, len_q, -1)  # b x lq x (n*dv)
+        output = output.permute(1, 2, 0, 3).contiguous().view(sz_b, len_q, -1)  
 
         output = self.dropout(self.fc(output))
         output = self.layer_norm(output + residual)
@@ -61,7 +59,6 @@ class MultiHeadAttention(nn.Module):
 
 
 class SDPAttention(nn.Module):
-    ''' Scaled Dot-Product Attention '''
 
     def __init__(self, temperature, attn_dropout=0.1):
         super().__init__()
